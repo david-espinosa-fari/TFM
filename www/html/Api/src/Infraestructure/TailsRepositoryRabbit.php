18d1c592995038d28393325bffa187ab5b9247e1
David-Espinosa-Fari <david.espinosa@students.salle.url.edu>
2019-08-21 19:41:55 +0200

    Creado evento para actualizar la cache cuando se busca una estacion, se ha modificado el repositorio de cache para el hash hacerlo alla abajo, de momento el evento es solo para un objeto de tipo estacion, falta añadirlo en los casos de uso de update una estacion, eliminar una estacion y crear una estacion, ademas faltaria incorporarlo en los find all

---
+++ www/html/Api/src/Infraestructure/TailsRepositoryRabbit.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Infraestructure;
+
+use App\Domain\Events\MeteosalleEvents;
+use App\Domain\TailMessageRepository;
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+
+class TailsRepositoryRabbit implements TailMessageRepository
+{
+
+	private $channel;
+	private $connection;
+
+	public function __construct()
+	{
+		$this->connection = new AMQPStreamConnection($_SERVER['HOST_RABBIT'], 5672, $_SERVER['RABBIT_USER'],
+			$_SERVER['RABBIT_PASS']);
+		$this->channel = $this->connection->channel();
+	}
+
+	public function publishEvent(MeteosalleEvents $event): void
+	{
+		$this->channel->exchange_declare((string)$event, 'fanout', false, false, false);
+		$data = $event->getDataAsJson();
+
+		$msg = new AMQPMessage($data);
+
+		$this->channel->basic_publish($msg, (string)$event);
+		//echo ' [x] Sent ', $data, "\n";
+	}
+
+	public function __destruct()
+	{
+		$this->channel->close();
+		$this->connection->close();
+	}
+}
