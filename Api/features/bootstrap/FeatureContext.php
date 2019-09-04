16f1431333f37c12dc3df9d9e64d661bf52291fb
David-Espinosa-Fari <david.espinosa@students.salle.url.edu>
2019-09-04 16:12:23 +0200

    Instalado behat, creados test de comportamiento, guzzle no tiene para convertir el json de la respuesta, nos vamos a http client symfony

---
+++ Api/features/bootstrap/FeatureContext.php
@@ -0,0 +1,111 @@
+<?php
+
+use Behat\Behat\Context\Context;
+use Behat\Behat\Tester\Exception\PendingException;
+use Behat\Gherkin\Node\PyStringNode;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ClientException;
+
+/**
+ * Defines application features from the specific context.
+ */
+class FeatureContext implements Context
+{
+    private $lastResponse;
+    private $lastStatusCode;
+    private $headerContentType;
+    private $headerAutentication;
+    private const ADDRESS_API = 'http://meteosalle.local/apiv1';
+
+    /**
+     * Initializes context.
+     *
+     * Every scenario gets its own context instance.
+     * You can also pass arbitrary arguments to the
+     * context constructor through behat.yml.
+     */
+    public function __construct()
+    {
+    }
+
+    /**
+     * @Given /^I do a "([^"]*)" request to "([^"]*)"$/
+     */
+    public function iDoARequestTo($method, $uri)
+    {
+
+        $client = new Client(['base_uri' => self::ADDRESS_API]);
+
+        try {
+            $request = $client->request($method, $uri);
+
+            $this->lastResponse = json_decode($request->getBody(),true);
+
+            $this->lastStatusCode = $request->getStatusCode();
+            $this->headerContentType = $request->getHeaderLine('Content-Type');
+        } catch (ClientException $exception) {
+            $this->lastResponse   = $exception->getResponse()->getBody();
+            $this->lastStatusCode = $exception->getResponse()->getStatusCode();
+        }
+    }
+
+    /**
+     * @Then /^the response code should be "([^"]*)"$/
+     */
+    public function theResponseCodeShouldBe($arg1)
+    {
+        if ($this->lastStatusCode != $arg1)
+        {
+            throw new Exception('Los codigos de estado no coinciden '.$arg1." vs ".$this->lastStatusCode);
+        }
+    }
+
+    /**
+     * @Given /^the response content type should be "([^"]*)"$/
+     */
+    public function theResponseContentTypeShouldBe($arg1)
+    {
+        if ('Content-Type: '.$this->headerContentType !== $arg1 )
+        {
+              throw new Exception('Los content-type.'.$arg1.' no coinciden'.$this->headerContentType);
+        }
+    }
+
+    /**
+     * @Given /^I create a user whit "([^"]*)" request to "([^"]*)" whit userName "([^"]*)" password "([^"]*)" and uuidUser "([^"]*)"$/
+     */
+    public function iCreateAUSerWhitRequestToWhitUserNamePasswordAndUuidUser($method, $uri, $user, $password, $uuidUser)
+    {
+
+        $client = new Client(['base_uri' => self::ADDRESS_API]);
+
+        try {
+            $request = $client->request($method, $uri, [
+            'form_params' => [
+                'userName' => $user,
+                'password' => $password,
+                'uuidUser' => $uuidUser,
+            ]
+]);
+
+            $this->lastResponse   = $request->getBody()->getContents();
+            $this->lastStatusCode = $request->getStatusCode();
+        } catch (ClientException $exception) {
+            $this->lastResponse   = $exception->getResponse()->getBody();
+            $this->lastStatusCode = $exception->getResponse()->getStatusCode();
+        }
+    }
+    //theResponseCodeShouldBe
+
+    /**
+     * @Given /^the response body should have "([^"]*)"$/
+     */
+    public function theResponseBodyShouldHave($arg1)
+    {
+        if (!isset($this->lastResponse[$arg1]))
+        {
+            throw new Exception('No existe token en la respuesta');
+        }
+    }
+
+}
---
+++ Api/features/list_user_repositories.feature
@@ -0,0 +1,20 @@
+Feature: List Api status
+  In order to list the endpoints API
+  As a api user
+  I want to list the resources of the Api
+
+  Scenario: when Api check status
+    Given I do a "GET" request to "http://meteosalle.local/apiv1/status"
+    Then the response code should be "200"
+    And the response content type should be "Content-Type: application/json"
+
+  Scenario: when want check routs
+    Given I do a "GET" request to "http://meteosalle.local/apiv1/stat"
+    Then the response code should be "404"
+
+  Scenario: when want login
+    Given I create a user whit "POST" request to "http://meteosalle.local/apiv1/user/login/" whit userName "12344" password "a" and uuidUser "12344"
+    Then the response code should be "404"
+
+
+
